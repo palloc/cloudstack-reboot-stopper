9caf901cd68922eb09b856143e2f33b9631dad07
palloc <palloc.mknet10@gmail.com>
2016-08-23 12:13:45 +0900

    fixed bug

--- stop-reboot.py
+++ stop-reboot.py
@@ -5,34 +5,47 @@ import datetime
 def Com_ack():
     loss_pat='0 packets received'
     management_ip = "127.0.0.1"
-    ping = subprocess.Popen(
-        ["ping","-c","1", "-i", "15", management_ip],
-        stdout = subprocess.PIPE,
-        stderr = subprocess.PIPE
-    )
-    out, error = ping.communicate()
-    message = ''
+    counter = 0
+    for i in range(3):
+        ping = subprocess.Popen(
+            ["ping","-c","1", "-i", "15", management_ip],
+            stdout = subprocess.PIPE,
+            stderr = subprocess.PIPE
+        )
+        out, error = ping.communicate()
+        message = ''
+        
+        for line in out.splitlines():
+            if loss_pat in line:
+                message = line.split(msg_pat)[1]
+                flag = False
+            else:
+                flag = True
+            if flag:
+                print '[ OK ] ' + 'Connection success to ' + management_ip + ' at ' + datetime.datetime.now().strftime('%x %X')
+            else:
+                print '[ NO ]' + datetime.datetime.now().strftime('%x %X')
+                counter += 1
+                break
+        if counter == 3:
+            agentstop = subprocess.Popen(
+                ["service", "cloudstack-agent", "stop"],
+                agentstdout = subprocess.PIPE,
+                agnetstderr = subprocess.PIPE
+            )
+            out, error = agentstop.communicate()
+            managementstop = subprocess.Popen(
+                ["service", "cloudstack-management", "stop"],
+                managestdout = subprocess.PIPE,
+                managestderr = subprocess.PIPE
+            )
+            out, error = agentstop.communicate()            
 
-    for line in out.splitlines():
-        if line.find(loss_pat)>-1:
-            message = line.split(msg_pat)[1]
-            flag=False
-            break
-    else:
-        flag = True
-    if flag:
-        print '[ OK ] ' + 'Connection success to ' + management_ip + ' at ' + datetime.datetime.now().strftime('%x %X')
-        return 0
-    else:
-        print '[ NO ]' + datetime.datetime.now().strftime('%x %X')
-    return 1
+
 
 if __name__ == '__main__':
-    counter = 0
+
     while True:
-        counter += Com_ack()
-        if counter > 2:
-            subprocess.call(["service", "cloudstack-agent", "stop"])
-            subprocess.call(["service", "cloudstack-management", "stop"])            
+        Com_ack()
         time.sleep(75)
-        print counter
+        
