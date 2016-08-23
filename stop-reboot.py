d3beb693f7a3a85009c3aa241d5ea59cabbc1ab2
palloc <palloc.mknet10@gmail.com>
2016-08-23 10:34:51 +0900

    fixed

--- stop-reboot.py
+++ stop-reboot.py
@@ -4,10 +4,9 @@ import datetime
 
 def Com_ack():
     loss_pat='0 packets received'
-    msg_pat='icmp_seq=1'
     management_ip = "127.0.0.1"
     ping = subprocess.Popen(
-        ["ping","-c","1", "-i", "5", management_ip],
+        ["ping","-c","1", "-i", "15", management_ip],
         stdout = subprocess.PIPE,
         stderr = subprocess.PIPE
     )
@@ -15,20 +14,25 @@ def Com_ack():
     message = ''
 
     for line in out.splitlines():
-        if line.find(msg_pat)>-1:
+        if line.find(loss_pat)>-1:
             message = line.split(msg_pat)[1]
-        if line.find(msg_pat)>-1:
             flag=False
             break
     else:
         flag = True
     if flag:
         print '[ OK ] ' + 'Connection success to ' + management_ip + ' at ' + datetime.datetime.now().strftime('%x %X')
+        return 0
     else:
         print '[ NO ]' + datetime.datetime.now().strftime('%x %X')
-        subprocess.call(["service", "cloudstack-agent", "stop"])
-        
+    return 1
+
 if __name__ == '__main__':
+    counter = 0
     while True:
-        Com_ack()
+        counter += Com_ack()
+        if counter > 2:
+            subprocess.call(["service", "cloudstack-agent", "stop"])
+            subprocess.call(["service", "cloudstack-management", "stop"])            
         time.sleep(75)
+        print counter
